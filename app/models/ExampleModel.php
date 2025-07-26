6f957af55938955d11499cd8255509b7f7236ae9
jshksjfdjfskdfsdfsdf432 <cmrecto007@gmail.com>
2025-07-26 23:14:03 +0800

    Delete app directory

--- app/models/ExampleModel.php
+++
@@ -1,6 +0,0 @@
-<?php
-class ExampleModel {
-    public function getMessage() {
-        return "Hello from MVC on Vercel!";
-    }
-}
--- app/views/home.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>MVC on Vercel</title>
-</head>
-<body>
-    <h1><?= htmlspecialchars($message) ?></h1>
-</body>
-</html>
